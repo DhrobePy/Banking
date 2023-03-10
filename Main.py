3031c33cc702cb06b51c453308679cbc5e7f2aa6
DhrobePy <107049606+DhrobePy@users.noreply.github.com>
2023-03-10 11:08:32 +0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,15 @@
 import streamlit as st
 
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+
 st.title("Banking Terms & Conditions")
 
 

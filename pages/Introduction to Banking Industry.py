d471f896bd7f39a8265495a1ade5cd278a24909e
DhrobePy <107049606+DhrobePy@users.noreply.github.com>
2023-03-10 11:06:41 +0600

    Update Introduction to Banking Industry.py

--- pages/Introduction to Banking Industry.py
+++ pages/Introduction to Banking Industry.py
@@ -26,10 +26,9 @@ if option == "BASEL-I":
         3. Uniform Standards: Basel I established uniform standards for banks across different countries, which helped to level the playing field and promote fair competition.
         
         
-""")                                                          
+""")
+    st.markdown("<h2 style='text-align: center;'>Drawback of BASEL-I</h2>", unsafe_allow_html=True)
     st.write("""
-    
-    Drawback of BASEL-I:
     
 The main drawback of Basel I was that it did not take into account the varying degrees of risk within each asset class. This meant that some assets with the same risk weight had very different levels of risk, which led to the underestimation of the actual risk held by banks. In response to this, Basel II was introduced in 2004, which incorporated more sophisticated risk measurement techniques and revised capital requirements.
 
@@ -45,6 +44,8 @@ elif option == "BASEL-II":
     st.write("This is option 2.")
     
 elif option == "Tier-1 Capital":
+    
+    st.markdown("<h1 style='text-align: center;'>Tier-1 Capital</h1>", unsafe_allow_html=True)
     st.write("""
     
     Tier-1 Capital:
@@ -57,6 +58,23 @@ elif option == "Tier-1 Capital":
             Additional Tier 1 capital includes instruments that can absorb losses but are more risky than CET1, such as perpetual preferred stock and contingent convertible bonds (CoCos). These instruments have features that allow them to be written down or converted to equity if the bank's capital falls below a certain level.
     
     Both CET1 and Additional Tier 1 capital are considered high-quality capital because they can absorb losses without causing a bank to become insolvent. Tier 1 capital is used to absorb losses before Tier 2 capital, which is a lower-quality form of capital.Overall, Tier 1 capital provides a measure of a bank's ability to withstand losses and continue operating without the need for external support. It is a key metric for regulators and investors in assessing a bank's financial strength and stability.
+    
+    """)
+    st.markdown("<h2 style='text-align: center;'>Components of Tier-1 Capital</h2>", unsafe_allow_html=True)
+    st.write("""
+    
+    Common Equity Tier 1 (CET1) capital is a core component of a bank's capital structure and is considered the highest quality form of capital. The components of CET1 capital include:
+
+    1. Common stock: Common stock represents ownership in the bank and is the most basic form of CET1 capital. It includes the par value of the bank's common shares, as well as any additional paid-in capital in excess of the par value.
+
+    2. Retained earnings: Retained earnings are profits that the bank has earned but has not distributed as dividends. Retained earnings are a key source of CET1 capital because they represent the bank's accumulated earnings that can be used to absorb losses.
+    
+    3. Other comprehensive income (OCI): OCI includes gains and losses that are not included in the bank's net income, such as unrealized gains or losses on investments or changes in the value of pension plans. OCI can be included in CET1 capital if it meets certain criteria.
+    
+    4. Minority interests: Minority interests refer to the portion of a subsidiary's equity that is not owned by the bank. If the bank owns less than 100% of a subsidiary, the minority interest can be included in CET1 capital if it meets certain criteria.
+
+
+    Overall, CET1 capital is a key measure of a bank's financial strength and is used by regulators to assess a bank's ability to absorb losses. The components of CET1 capital represent the highest quality form of capital because they are fully loss-absorbing and can be used to absorb losses without causing the bank to become insolvent.
     
     """)
 

47171df03146576f95d6008adf63fce1117c2a8e
DhrobePy <107049606+DhrobePy@users.noreply.github.com>
2023-03-10 09:37:57 +0600

    Update General Banking.py

--- pages/General Banking.py
+++ pages/General Banking.py
@@ -3,16 +3,23 @@ import streamlit as st
 st.write("Welcome to General Banking Page. Please select desired options in sidebar to explore GB related queries")
 
 
-options = ["Negotiable Instument Act"]
+# Define a list of options
+options = ["Negotiable Instrument Act"]
 
 # Define a function to add a new option to the list
 def add_option(new_option):
     options.append(new_option)
+    st.session_state.options = options  # Update session state
+
+# Initialize session state
+if "options" not in st.session_state:
+    st.session_state.options = options
 
 # Add the form to the sidebar
-new_option = st.sidebar.text_input("Add a new Topic:")
+new_option = st.sidebar.text_input("Add a Topic:")
 if st.sidebar.button("Add"):
     add_option(new_option)
 
 # Display the list of options in the main content area
-st.sidebar.radio("Topics",options)
+st.write("Options:", st.session_state.options)
+

1e349de56565601fb50a6c06ecbf9a2d99277f24
DhrobePy <107049606+DhrobePy@users.noreply.github.com>
2023-03-10 10:48:25 +0600

    Update Introduction to Banking Industry.py

--- pages/Introduction to Banking Industry.py
+++ pages/Introduction to Banking Industry.py
@@ -1,25 +1,22 @@
 import streamlit as st
 
 # Define a list of options
-options = ["BASEL-I", "BASEL-II", "BASEL-III"]
+options = ["BASEL-I", "BASEL-II", "BASEL-III", "Tier-1 Capital", "Tier-2 Capital"]
 
 # Add the options to the sidebar
 option = st.sidebar.selectbox("Select a Topic", options)
 
 # Display the selected option
 if option == "BASEL-I":
+    BASEL-I()
+def BASEL-I():
     st.markdown("<h1 style='text-align: center;'>BASEL-I</h1>", unsafe_allow_html=True)
-
     st.write("""
-    
-    Basel I, also known as the Basel Accord, was developed in 1988 by the Basel Committee on Banking Supervision. The accord aimed to establish a set of international banking standards to ensure banks had enough capital to withstand financial shocks and reduce the risk of bank failures.
-    
+    Basel I, also known as the Basel Accord, was developed in 1988 by the Basel Committee on Banking Supervision. The accord aimed to establish a set of international banking standards to ensure banks had enough capital to withstand financial shocks and reduce the risk of bank failures.  
     The key features of Basel I include:
-    
         1. Minimum Capital Requirement: Basel I introduced a minimum capital requirement of 8% of a bank's risk-weighted assets. Banks were required to hold this capital in the form of Tier 1 and Tier 2 capital.
         The calculation of CAR under Basel I can be simplified as follows:
             CAR = (Tier 1 Capital + Tier 2 Capital) / Risk-Weighted Assets
-        
         2. Risk Weighted Assets: Basel I classified assets into five risk categories and assigned them risk weights ranging from 0% to 100%. The risk weights were used to calculate the amount of capital a bank was required to hold against its assets.
         3. Uniform Standards: Basel I established uniform standards for banks across different countries, which helped to level the playing field and promote fair competition.
         

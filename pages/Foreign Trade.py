4c9d68818a68afd12504914b47dd1ed4699310e6
DhrobePy <107049606+DhrobePy@users.noreply.github.com>
2023-03-10 12:43:59 +0600

    Update Foreign Trade.py

--- pages/Foreign Trade.py
+++ pages/Foreign Trade.py
@@ -16,7 +16,7 @@ option = st.sidebar.selectbox("Select a Topic", options)
 if option == "IncoTerms-2020":
   st.markdown("<h1 style='text-align: center;'>IncoTerms-2020</h1>", unsafe_allow_html=True)
 
-  st.image('https://github.com/DhrobePy/Banking/blob/5e7633e670cd56077b7d3d22f0ed3ad8071c9109/incoterm.json', caption='Simple Explanation of IncoTerms-2020')
+  st.image('https://myseatime.com/blogadm/wp-content/uploads/2015/12/Incoterms.jpg', caption='Simple Explanation of IncoTerms-2020')
   
   st.write("""
            Incoterms, or International Commercial Terms, are standardized terms used in international trade to define the responsibilities of buyers and sellers with regard to the delivery of goods. The latest version of Incoterms, Incoterms 2020, came into effect on January 1, 2020. Here is a list of the 11 Incoterms 2020, along with a brief explanation of each:

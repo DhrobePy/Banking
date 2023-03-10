6387b094244069f9923bc591ccb7387c099037f6
DhrobePy <107049606+DhrobePy@users.noreply.github.com>
2023-03-10 12:42:45 +0600

    Update Foreign Trade.py

--- pages/Foreign Trade.py
+++ pages/Foreign Trade.py
@@ -13,26 +13,10 @@ options = ["IncoTerms-2020"]
 option = st.sidebar.selectbox("Select a Topic", options)
 
 
-
 if option == "IncoTerms-2020":
   st.markdown("<h1 style='text-align: center;'>IncoTerms-2020</h1>", unsafe_allow_html=True)
-  
-  url = "https://github.com/DhrobePy/Banking/blob/d04a01f4d94acf8bbca24e169154fca3c7fd0e74/incoterm.json"
 
-# Send a GET request to retrieve the JSON data
-  response = requests.get(url)
-
-# Load the JSON data from the response content
-  data = response.json()
-
-# Display the JSON data in a Streamlit component
-  st.json(data)
-  
-  
-
-# Display the JSON data in a Streamlit component
-  #st.json(data)
-  #st.image('https://github.com/DhrobePy/Banking/blob/5e7633e670cd56077b7d3d22f0ed3ad8071c9109/incoterm.json', caption='Simple Explanation of IncoTerms-2020')
+  st.image('https://github.com/DhrobePy/Banking/blob/5e7633e670cd56077b7d3d22f0ed3ad8071c9109/incoterm.json', caption='Simple Explanation of IncoTerms-2020')
   
   st.write("""
            Incoterms, or International Commercial Terms, are standardized terms used in international trade to define the responsibilities of buyers and sellers with regard to the delivery of goods. The latest version of Incoterms, Incoterms 2020, came into effect on January 1, 2020. Here is a list of the 11 Incoterms 2020, along with a brief explanation of each:

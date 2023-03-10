7e528e368c65076696e89ce45d3e09e1f19b35e8
DhrobePy <107049606+DhrobePy@users.noreply.github.com>
2023-03-10 09:31:31 +0600

    Update General Banking.py

--- pages/General Banking.py
+++ pages/General Banking.py
@@ -1,3 +1,18 @@
 import streamlit as st
 
 st.write("Welcome to General Banking Page. Please select desired options in sidebar to explore GB related queries")
+
+
+options = ["Negotiable Instument Act"]
+
+# Define a function to add a new option to the list
+def add_option(new_option):
+    options.append(new_option)
+
+# Add the form to the sidebar
+new_option = st.sidebar.text_input("Add a new Topic:")
+if st.sidebar.button("Add"):
+    add_option(new_option)
+
+# Display the list of options in the main content area
+st.sidebar("Topics", options)

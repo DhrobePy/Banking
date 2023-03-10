1c081ab7f4fb58929c41aeb3799e11210e6e93b5
DhrobePy <107049606+DhrobePy@users.noreply.github.com>
2023-03-10 19:34:22 +0600

    Update General Banking.py

--- pages/General Banking.py
+++ pages/General Banking.py
@@ -8,7 +8,37 @@ option = st.sidebar.selectbox("Select a Topic", options)
 
 if option=="Negotiable Instrument Act":
     
+    st.markdown("<h1 style='text-align: center;'>Negotiable Instrument Act</h1>", unsafe_allow_html=True)
     st.write("""
+    The Negotiable Instruments Act, 1881 was enacted in India on 9 December 1881 and came into effect on 1 March 1882. The act was introduced in response to the need for a uniform law governing negotiable instruments, such as promissory notes, bills of exchange, and cheques, which were widely used in commercial transactions.
+
+Prior to the enactment of the act, the laws governing negotiable instruments were based on English common law and various local customs and practices. This led to a lack of uniformity and uncertainty in the law, which hindered the development of trade and commerce.
+
+To address this issue, the Indian Law Commission was appointed in 1861 to review and recommend changes to the laws related to negotiable instruments. The Commission submitted its report in 1867, which formed the basis for the eventual enactment of the Negotiable Instruments Act, 1881.
+
+The act was based on the English Bills of Exchange Act, 1882, which was enacted a year later. The Indian act, however, made certain modifications to the English act to suit the Indian context, such as recognizing hundis and other indigenous instruments of credit.
+
+Over the years, the act has been amended several times to reflect changes in commercial practices and to address practical issues that have arisen in its implementation. The most recent amendments were made in 2018 to improve the efficiency and efficacy of cheque bouncing cases and to enhance the credibility of negotiable instruments.
+
+The Negotiable Instruments Act, 1881, which was enacted in India, also applies to Bangladesh. However, the act has been amended in Bangladesh over the years to reflect changes in commercial practices and to address practical issues that have arisen in its implementation.
+
+In 1899, the British colonial government in India extended the Negotiable Instruments Act, 1881 to Bangladesh, which was then known as East Bengal and Assam. The act was subsequently adopted by Pakistan after the partition of India in 1947, and it continued to apply to Bangladesh after it gained independence from Pakistan in 1971.
+
+In 1989, the Bangladeshi government enacted the Negotiable Instruments Act, 1989, which repealed and replaced the original act. The 1989 act was based on the Indian act, but it made some modifications to suit the Bangladeshi context.
+
+In 2006, the Bangladeshi government amended the act to enhance its effectiveness and efficiency in dealing with cheque bouncing cases. The amendment introduced provisions for the speedy disposal of such cases and for the establishment of special courts to deal with them.
+
+In 2018, the Bangladeshi government further amended the act to address issues related to electronic banking and to facilitate the use of electronic instruments for commercial transactions. The amendment also introduced provisions for the protection of payees and the enhancement of the credibility of negotiable instruments in Bangladesh.
+
+Overall, the history of the Negotiable Instruments Act in Bangladesh reflects the country's efforts to adapt and improve its legal framework to keep pace with the changing needs of its commercial sector.
+    
+    """)
+    
+    st.markdown("<h2 style='text-align: center;'>Sections</h2>", unsafe_allow_html=True)
+    st.write("""
+    
+    The List of sections in NI act as follows, 
+    
 1.	Short title
 2.	Interpretation clause
 3.	Promissory note

33f666761888f36f30be862fa9c6ef232b766a13
DhrobePy <107049606+DhrobePy@users.noreply.github.com>
2023-03-10 19:21:29 +0600

    Update General Banking.py

--- pages/General Banking.py
+++ pages/General Banking.py
@@ -5,21 +5,79 @@ st.write("Welcome to General Banking Page. Please select desired options in side
 
 # Define a list of options
 options = ["Negotiable Instrument Act"]
+# Add the options to the sidebar
+option = st.sidebar.selectbox("Select a Topic", options)
 
-# Define a function to add a new option to the list
-def add_option(new_option):
-    options.append(new_option)
-    st.session_state.options = options  # Update session state
+if option=="Negotiable Instrument Act":
+    
+    st.write("""
+    
+    Short title
+Interpretation clause
+"Promissory note"
+"Bill of exchange"
+"Cheque"
+"Holder"
+"Holder in due course"
+"Value"
+"Instrument"
+"Payable on demand"
+"At a fixed time"
+"Writing"
+Negotiable instruments
+Negotiation of negotiable instruments
+Endorsement
+Endorsement in blank and special endorsement
+Effect of endorsement
+Transferor by delivery and transferee
+Negotiation of instrument payable to bearer
+Rights of holder in due course
+Effect of material alteration
+Liability of maker, drawer and acceptor of negotiable instrument
+When the maker of a note is deemed to be an indorser
+Liability of indorser
+Liability of drawer of cheque
+Liability of drawee of cheque
+Payment in due course of negotiable instrument
+Payment to a banker
+Payment by whom?
+Discharge of indorser's liability
+Discharge from liability
+Exoneration of prior parties
+Holder's right to duplicate of lost bill
+Protection to bankers paying cheques bearing forged endorsements
+"Crossing" of cheques
+"Cheques" crossed specially
+General crossing defined and described
+Payment of crossed cheque
+Duties of banker on whom crossed cheque is drawn
+Protection of paying banker
+Collection of cheque by banker
+"Notice of dishonour"
+By and to whom notice should be given
+Mode in which notice may be given
+Time within which notice must be given
+Delay in giving notice
+Notice of dishonour necessary
+Excuse for delay in presentment for payment
+Presentment of cheque to charge drawer
+Presentment for payment
+When presentment unnecessary
+Liability of maker of note and acceptor of bill who fail to present on due date
+When note or bill matures on holiday
+When day of maturity is a holiday
+Applicability of the Indian Limitation Act, 1963
+Accommodation bills and notes
+Discharge of liability by payment in due course
+Payment to agent
+Payment to one of several joint payees
+Payment to banker under banker's lien
+Payment to rightful owner
+Savings of presentment for payment and notice of dishonour
+Repeal of Acts
+Savings of certain enactments
+Power to make rules
+This list covers all the sections in the Negotiable Instruments Act.
 
-# Initialize session state
-if "options" not in st.session_state:
-    st.session_state.options = options
-
-# Add the form to the sidebar
-new_option = st.sidebar.text_input("Add a Topic:")
-if st.sidebar.button("Add"):
-    add_option(new_option)
-
-# Display the list of options in the main content area
-st.radio("Topics:", st.session_state.options)
-
+""")
+    

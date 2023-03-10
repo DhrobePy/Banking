cde3c9b51b0bacd04c24f84d76bb1422f152ac15
DhrobePy <107049606+DhrobePy@users.noreply.github.com>
2023-03-10 09:01:44 +0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ st.title("Banking Terms & Conditions")
 options = ["Introduction to Banking ","Genearal Banking", "Credit", "Foreign Trade"]
 
 # Add the radio buttons to the sidebar
-selected_option = st.sidebar.radio("Select an option", options)
+#selected_option = st.sidebar.radio("Select an option", options)
 
 # Display the selected option in the main content area
-st.write("You selected:", selected_option)
+#st.write("You selected:", selected_option)
